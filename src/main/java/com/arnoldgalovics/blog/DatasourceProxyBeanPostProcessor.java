0ec15e997ad00805b50fa6a55f22475b357090d5
Arnold Galovics <galovicsarnold@gmail.com>
2017-08-04 10:44:23 +0200

    Added support for Spring Boot 2.0.0.M3 as it was failing

--- src/main/java/com/arnoldgalovics/blog/DatasourceProxyBeanPostProcessor.java
+++ src/main/java/com/arnoldgalovics/blog/DatasourceProxyBeanPostProcessor.java
@@ -1,10 +1,16 @@
 package com.arnoldgalovics.blog;
 
+import java.lang.reflect.Method;
+
 import javax.sql.DataSource;
 
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
 
 import net.ttddyy.dsproxy.listener.logging.SLF4JLogLevel;
 import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
@@ -20,10 +26,29 @@ public class DatasourceProxyBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessAfterInitialization(final Object bean, final String beanName) throws BeansException {
         if (bean instanceof DataSource) {
-            DataSource dataSourceBean = (DataSource) bean;
-            return ProxyDataSourceBuilder.create(dataSourceBean).logQueryBySlf4j(SLF4JLogLevel.INFO).name("dataSource").countQuery().build();
+            ProxyFactory factory = new ProxyFactory(bean);
+            factory.setProxyTargetClass(true);
+            factory.addAdvice(new ProxyDataSourceInterceptor((DataSource) bean));
+            return factory.getProxy();
         }
         return bean;
     }
 
+    private static class ProxyDataSourceInterceptor implements MethodInterceptor {
+        private final DataSource dataSource;
+
+        public ProxyDataSourceInterceptor(final DataSource dataSource) {
+            super();
+            this.dataSource = ProxyDataSourceBuilder.create(dataSource).countQuery().logQueryBySlf4j(SLF4JLogLevel.INFO).build();
+        }
+
+        @Override
+        public Object invoke(final MethodInvocation invocation) throws Throwable {
+            Method proxyMethod = ReflectionUtils.findMethod(dataSource.getClass(), invocation.getMethod().getName());
+            if (proxyMethod != null) {
+                return proxyMethod.invoke(dataSource, invocation.getArguments());
+            }
+            return invocation.proceed();
+        }
+    }
 }
